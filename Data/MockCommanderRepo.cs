29615ab7d28b4ce28f689a3fee14b5f332cbacc3
Donovan Hurter <donovanhurter@outlook.com>
2021-06-25 18:07:42 -0500

    Add project files.

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & Pan"},
+                new Command{Id = 1, HowTo = "Cut break", Line = "Get Knife", Platform = "Knife and chopping board"},
+                new Command{Id = 2, HowTo = "Make a cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & Pan"};
+        }
+    }
+}
